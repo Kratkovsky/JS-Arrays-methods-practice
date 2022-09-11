799ec1a90ad6a5c28bcba810cb8cb234600329b2
Kratkovky <kkratkovsky@gmail.com>
2022-09-11 19:05:18 +0300

    add 2 task

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
+// 1. Даны два массива: [1, 2, 3] и [4, 5, 6]. Объедините их вместе.
+
 const first =  [1, 2, 3],
 second = [4, 5, 6]; 
-
 const fused = first.concat(second);
 
 console.log(fused);
+
+
+// 2. Дан массив [1, 2, 3]. Сделайте из него массив [3, 2, 1].
+
+const array = [1, 2, 3];
+array.reverse();
+
+console.log(array);
+
+
+// 3. 
